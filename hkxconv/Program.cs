53c1c9859d5ea8f723cd7ed8fe109ee95d77cfba
ret2end <ret2end@proton.me>
2023-01-01 21:03:18 +0800

    init

---
+++ hkxconv/Program.cs
@@ -0,0 +1,37 @@
+﻿using hkxconv.cmd;
+using System.CommandLine;
+
+namespace hkxconv
+{
+
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var rootCommand = new RootCommand("Tool for convert HKX file to XML");
+
+
+            var convertFormatOpt = new Option<ConvertFormat>(name: "-v", description: "Convert format.", getDefaultValue: () => ConvertFormat.xml);
+            var verboseOpt = new Option<bool>(name: "--verbose", description: "verbose", getDefaultValue: () => false);
+            var inputDirArg = new Argument<FileSystemInfo>("Input dir or file");
+            var outputDirArg = new Argument<FileSystemInfo>("Output dir or file", getDefaultValue: () => new DirectoryInfo("."));
+
+            var convertCommand = new Command("convert", "Convert hkx file to other format")
+            {
+                convertFormatOpt,
+                verboseOpt,
+                inputDirArg,
+                outputDirArg,
+            };
+
+            rootCommand.Add(convertCommand);
+
+            convertCommand.SetHandler((format, verbose, input, output) =>
+            {
+                cmd.Convert.ToFormat(input, output, format, verbose);
+            }, convertFormatOpt, verboseOpt, inputDirArg, outputDirArg);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+    }
+}
---
+++ hkxconv/cmd/Convert.cs
@@ -0,0 +1,97 @@
+﻿using HKX2;
+
+namespace hkxconv.cmd
+{
+    public enum ConvertFormat
+    {
+        xml = 0,
+        //amd64 = 1,
+        //win32 = 2,
+    }
+    public class Convert
+    {
+        public static int ToFormat(FileSystemInfo input, FileSystemInfo output, ConvertFormat format, bool verbose)
+        {
+            if (!input.Exists)
+            {
+                Console.WriteLine("Input file not found.");
+                return -1;
+            }
+            int retCode = -1;
+            switch ((input, output))
+            {
+                case (FileInfo inFile, FileInfo outFile):
+                    retCode = ConvertHKX(inFile, outFile, format, verbose);
+                    break;
+                case (FileInfo inFile, DirectoryInfo outDir):
+                    retCode = ConvertHKX(inFile, outDir, format, verbose);
+                    break;
+                case (DirectoryInfo inDir, DirectoryInfo outDir):
+                    retCode = ConvertHKX(inDir, outDir, format, verbose);
+
+                    break;
+                default:
+                    break;
+            }
+            return retCode;
+        }
+
+        public static int ConvertHKX(DirectoryInfo inDir, DirectoryInfo outDir, ConvertFormat format, bool verbose)
+        {
+            if (!outDir.Exists)
+                Directory.CreateDirectory(outDir.Name);
+
+            int retCode;
+            foreach (var item in Directory.EnumerateFiles(inDir.ToString(), "*.hkx", SearchOption.AllDirectories))
+            {
+                var relativePath = Path.GetRelativePath(inDir.FullName, item);
+                var inFile = new FileInfo(Path.Combine(inDir.ToString(), relativePath));
+                var outFile = new FileInfo(Path.Combine(outDir.ToString(), Path.ChangeExtension(relativePath, format.ToString())));
+
+                retCode = ConvertHKX(inFile, outFile, format, verbose);
+                if (retCode != 0)
+                {
+                    return retCode;
+                }
+
+            }
+            return 0;
+        }
+
+        public static int ConvertHKX(FileInfo inFile, DirectoryInfo outDir, ConvertFormat format, bool verbose)
+        {
+            if (!outDir.Exists)
+                Directory.CreateDirectory(outDir.Name);
+
+            var outFile = new FileInfo(Path.Combine(outDir.ToString(), Path.ChangeExtension(inFile.Name, format.ToString())));
+            return ConvertHKX(inFile, outFile, format, verbose);
+        }
+
+        public static int ConvertHKX(FileInfo inFile, FileInfo outFile, ConvertFormat format, bool verbose)
+        {
+            if (verbose)
+                Console.WriteLine($"Convert {inFile} to {outFile}");
+            if (!outFile.Exists)
+                if (outFile.DirectoryName is null)
+                    throw new Exception("DirectoryName is null?");
+                else
+                    Directory.CreateDirectory(outFile.DirectoryName);
+
+            HKXHeader header = (format) switch
+            {
+                ConvertFormat.xml => HKXHeader.SkyrimSE(),
+                _ => throw new NotImplementedException("no format found"),
+            };
+
+            var br = new BinaryReaderEx(inFile.OpenRead());
+            var des = new PackFileDeserializer();
+            IHavokObject rootObject = des.Deserialize(br);
+
+            var xs = new XmlSerializer();
+
+            xs.Serialize(rootObject, header, outFile.Create());
+
+            return 0;
+        }
+    }
+}
