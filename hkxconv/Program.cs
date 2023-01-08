bad093d6bf9a831197079bf594e5f63aafc56386
ret2end <ret2end@proton.me>
2023-01-08 23:22:12 +0800

    add --ignore-cast-error flag

--- hkxconv/Program.cs
+++ hkxconv/Program.cs
@@ -13,7 +13,7 @@ namespace hkxconv
 
             var convertFormatOpt = new Option<ConvertFormat>(name: "-v", description: "Convert format.", getDefaultValue: () => ConvertFormat.xml);
             var verboseOpt = new Option<bool>(name: "--verbose", description: "verbose", getDefaultValue: () => false);
-            //var ignoreErrorOpt = new Option<bool>(name: "--quiet", description: "ingore non-fatal error(content will be different from original)", getDefaultValue: () => false);
+            var ignoreErrorOpt = new Option<bool>(name: "--ignore-cast-error", description: "ignore fail to cast error(output will missing data. use with caution.)", getDefaultValue: () => false);
             var inputDirArg = new Argument<FileSystemInfo>("Input dir or file");
             var outputDirArg = new Argument<FileSystemInfo>("Output dir or file", getDefaultValue: () => new DirectoryInfo("."));
 
@@ -21,17 +21,17 @@ namespace hkxconv
             {
                 convertFormatOpt,
                 verboseOpt,
-                //ignoreErrorOpt,
+                ignoreErrorOpt,
                 inputDirArg,
                 outputDirArg,
             };
 
             rootCommand.Add(convertCommand);
 
-            convertCommand.SetHandler((format, verbose, input, output) =>
+            convertCommand.SetHandler((format, verbose, ignoreErrorOpt, input, output) =>
             {
-                cmd.Convert.ToFormat(input, output, format, verbose, false);
-            }, convertFormatOpt, verboseOpt, inputDirArg, outputDirArg);
+                cmd.Convert.ToFormat(input, output, format, verbose, ignoreErrorOpt);
+            }, convertFormatOpt, verboseOpt, ignoreErrorOpt, inputDirArg, outputDirArg);
 
             return await rootCommand.InvokeAsync(args);
         }
